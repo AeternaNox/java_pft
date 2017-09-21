64ed937b81740d53d5cb0274aefb3eb99057e5d1
AeternaNox <antares87@mail.ru>
2017-09-21 16:30:36 +0300

    Настройки

--- Sandbox/src/main/java/ru/stqa/pft/samdbox/MyFirstProgram.java
+++ Sandbox/src/main/java/ru/stqa/pft/samdbox/MyFirstProgram.java
@@ -1,9 +1,9 @@
 package ru.stqa.pft.samdbox;
 
 public class MyFirstProgram {
-	
-	public static void main(String[] args) {
-		System.out.println("Hellow, word!");
-	}
-	
+
+  public static void main(String[] args) {
+    System.out.println("Hellow, word!");
+  }
+
 }	
