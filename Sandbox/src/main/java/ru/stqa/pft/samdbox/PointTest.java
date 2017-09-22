6384c6f26ec235d82e130cda784a6c47b9a039a8
AeternaNox <antares87@mail.ru>
2017-09-22 13:59:18 +0300

    Вычисление расстояния между двумя точками на плоскости

---
+++ Sandbox/src/main/java/ru/stqa/pft/samdbox/PointTest.java
@@ -0,0 +1,21 @@
+package ru.stqa.pft.samdbox;
+
+public class PointTest {
+  public static void main(String[] args) {
+    double x1 = 2;
+    double y1 = 3;
+    double x2 = 4;
+    double y2 = 8;
+
+    System.out.println("Проверка расстояния между точками: p1 (" + x1 + ", " + y1 + ") и p2 (" + x2 + ", " + y2 + ") равно " + distances(x1, y1, x2, y2));
+
+
+    Point p = new Point(2, 3, 4, 8);
+    System.out.println("Расстояние между точками: p1 (" + p.x1 + ", " + p.y1 + ") и p2 (" + p.x2 + ", " + p.y2 + ") равно " + p.distance());
+
+  }
+
+  public static double distances(double x1, double y1, double x2, double y2) {
+    return  Math.sqrt((x2-x1)*(x2-x1)+(y2-y1)*(y2-y1));
+  }
+}
